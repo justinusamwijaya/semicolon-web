7e37d1e4c01ee0cf8e2e08a6f760d091c597199f
justinusamwijaya <justin.am.wijaya@gmail.com>
2024-07-14 16:53:18 +0700

    fix typewriter animation bug

--- scripts.js
+++ scripts.js
@@ -13,27 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
       window.scrollTo(0, 0);
     }, 50);
   }, 10); // Adjusted to match the total animation time
-
-  // Typing animation
-  function typeWriter(text, i, fnCallback) {
-    if (i < text.length) {
-      document.getElementById("slogan").innerHTML =
-        text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-
-      setTimeout(function () {
-        typeWriter(text, i + 1, fnCallback);
-      }, 50);
-    } else if (typeof fnCallback == "function") {
-      setTimeout(fnCallback, 50);
-    }
-  }
-
-  // Start the typing animation after the initial fade-in
-  setTimeout(function () {
-    typeWriter("Driven by Innovation, Built on Trust", 0, function () {
-      // Animation is complete
-    });
-  }, 1000); // Adjust this delay as needed to match your fade-in duration
 });
 
 // Navbar scroll effect
